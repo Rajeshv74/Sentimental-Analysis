fedb066e27cc03484c7b6dd5cd05b05c67a997de
Raju <rajrajesh0492@gmail.com>
2026-05-06 21:07:53 +0530

    docker added

--- server.py
+++ server.py
@@ -454,6 +454,6 @@ def fallback_analysis(text):
 # ==============================
 
 if __name__ == "__main__":
-    print("🚀 Server running on http://127.0.0.1:5000")
-    app.run(debug=True)
+    print("🚀 Server running on http://0.0.0.0:5000")
+    app.run(host="0.0.0.0", port=5000, debug=True)
 
